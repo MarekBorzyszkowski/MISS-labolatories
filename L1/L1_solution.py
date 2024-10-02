6791b61e910d9252f0900d86f1e1712c0ff6d60e
marek <31738037+MarekBorzyszkowski@users.noreply.github.com>
2024-10-02 09:03:53 +0200

    done

--- L1/L1_solution.py
+++ L1/L1_solution.py
@@ -7,10 +7,10 @@ ALLOWED = "allowed"
 FORBIDDEN = "forbidden"
 GRAVITY = 10
 
-regions = {ALLOWED: [(2, 1, 2, 4), (4, 1, 7, 1)], FORBIDDEN: [(3, 2, 3, 3)]}
+regions = {ALLOWED: [(2, 2, 2, 11), (7, 2, 7, 11)], FORBIDDEN: [(3, 5, 6, 5), (3, 8, 6, 8)]}
 
 v_0min_val = 1
-v_0max_val = 15
+v_0max_val = 30
 alpha_min_val = 1
 alpha_max_val = 89
 number_of_tries_val = 100000
@@ -18,12 +18,20 @@ number_of_tries_val = 100000
 # v_0_test, alpha_test = 9.85, 68
 # v_0_test, alpha_test = 12.013667928162864, 32.61695072706466
 
+linspace_angle = np.linspace(alpha_min_val, alpha_max_val, number_of_tries_val)
+linspace_v_0 = np.linspace(v_0min_val, v_0max_val, number_of_tries_val)
 
 throws = random_test_in_regions(v_0min_val, v_0max_val, alpha_min_val, alpha_max_val, number_of_tries_val, regions)
 fig1, ax1 = plt.subplots()
 ax1.set_xlim([0, 90])
 ax1.set_ylim([0, v_0max_val + 1])
+ax1.set_xlabel('α')
+ax1.set_ylabel('v_0')
 ax1.plot(*zip(*throws), ".")
+ax1.plot(linspace_angle, [v_0min_val] * len(linspace_angle), 'g')
+ax1.plot(linspace_angle, [v_0max_val] * len(linspace_angle), 'g')
+ax1.plot([alpha_min_val] * len(linspace_v_0), linspace_v_0, 'g')
+ax1.plot([alpha_max_val] * len(linspace_v_0), linspace_v_0, 'g')
 fig1.show()
 
 fig2, ax2 = plt.subplots()
@@ -41,7 +49,12 @@ for allowed in regions[ALLOWED]:
 for forbidden in regions[FORBIDDEN]:
     region_x0, region_y0, region_x1, region_y1 = forbidden
     ax2.plot([region_x0, region_x1], [region_y0, region_y1], "r")
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
 fig2.show()
+
+# results for exercise 3
+print(len(throws) / number_of_tries_val)
 
 # print(is_throw_valid(v_0_test, alpha_test))
 # for region_category in regions:
